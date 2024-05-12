b24ed05d7bf0d6607c9e4e439123f057223571d6
gabriel natan <gabriel--natan@live.com>
2024-05-12 13:09:08 -0300

    add new images

--- src/components/Partners/index.tsx
+++ src/components/Partners/index.tsx
@@ -1,3 +1,12 @@
+import Amazon from "/src/assets/Company=Amazon.png"
+import Dropbox from "/src/assets/Company=Dropbox.png"
+import Netflix from "/src/assets/Company=Netflix.png"
+import PayPal from "/src/assets/Company=PayPal.png"
+import Slack from "/src/assets/Company=Slack.png"
+import Spotify from "/src/assets/Company=Spotify.png"
+import Tesla from "/src/assets/Company=Tesla.png"
+import YouTube from "/src/assets/Company=YouTube.png"
+
 export const Partners = ()=>{
   return(
     <section className="w-full flex justify-center py-10 lg:py-28 bg-slate-100">
@@ -6,28 +15,28 @@ export const Partners = ()=>{
       <h2 className="text-4xl font-extrabold text-center sticky lg:relative left-0">Our Partners</h2>
       <div className=" flex w-[300vh] px-5   lg:w-full lg:grid lg:grid-rows-2 lg:grid-cols-4 gap-5">
         <div className="flex items-center justify-center w-full border rounded-lg p-5 bg-white">
-          <img src="/src/assets/Company=Amazon.png" className="max-w-40 w-full" />
+          <img src={Amazon} className="max-w-40 w-full" />
         </div>
         <div className="flex items-center justify-center w-full border rounded-lg p-5 bg-white">
-          <img src="/src/assets/Company=Dropbox.png" className="max-w-40 w-full object-contain" />
+          <img src={Dropbox} className="max-w-40 w-full object-contain" />
         </div>
         <div className="flex items-center justify-center w-full border rounded-lg p-5 bg-white">
-          <img src="/src/assets/Company=Netflix.png" className="max-w-40 w-full object-contain" />
+          <img src={Netflix} className="max-w-40 w-full object-contain" />
         </div>
         <div className="flex items-center justify-center w-full border rounded-lg p-5 bg-white">
-          <img src="/src/assets/Company=PayPal.png" className="max-w-40 w-full object-contain" />
+          <img src={PayPal} className="max-w-40 w-full object-contain" />
         </div>
         <div className="flex items-center justify-center w-full border rounded-lg p-5 bg-white">
-          <img src="/src/assets/Company=Slack.png" className="max-w-40 w-full object-contain" />
+          <img src={Slack} className="max-w-40 w-full object-contain" />
         </div>
         <div className="flex items-center justify-center w-full border rounded-lg p-5 bg-white">
-          <img src="/src/assets/Company=Spotify.png" className="max-w-40 w-full object-contain" />
+          <img src={Spotify} className="max-w-40 w-full object-contain" />
         </div>
         <div className="flex items-center justify-center w-full border rounded-lg p-5 bg-white">
-          <img src="/src/assets/Company=Tesla.png" className="max-w-40 w-full object-contain" />
+          <img src={Tesla} className="max-w-40 w-full object-contain" />
         </div>
         <div className="flex items-center justify-center w-full border rounded-lg p-5 bg-white">
-          <img src="/src/assets/Company=YouTube.png" className="max-w-40 w-full object-contain" />
+          <img src={YouTube} className="max-w-40 w-full object-contain" />
         </div>
       </div>
 
