7bcc8552cb0f8abe21320834af1d9dab8404c5a5
gabriel natan <gabriel--natan@live.com>
2024-05-12 13:04:33 -0300

    add logo

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -1,10 +1,10 @@
 import { Button } from "../Button"
-
+import logo from "/src/assets/logo.png"
 export const Navbar = ()=>{
   return(
     <div className="flex-1 container flex items-center justify-center lg:justify-between">
       <div className="flex items-center">
-        <img src="/src/assets/logo.png" className="w-14"/>
+        <img src={logo} className="w-14"/>
         <h1 className="text-xl font-extrabold text-purple-500">Front<span className="text-amber-500">React</span></h1>
       </div>
       <div className="fixed lg:relative top-0 left-0 h-screen lg:h-full w-screen lg:w-full bg-white lg:bg-transparent z-10 px-5  py-10 lg:p-0 translate-x-full lg:translate-x-0 flex flex-col lg:flex-row lg:justify-end lg:items-center">
